7ab4d389dae1edbd08497dad47e3193fccc5a3ac
dc0dr <ecdaniels826@outlook.com>
2026-04-10 09:09:10 +0000

    feat: add environment-based destination directory and implement file move functionality to file_uploader

--- file_uploader.py
+++ file_uploader.py
@@ -1,6 +1,11 @@
 import os 
 import shutil
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DESTINATION_DIR = os.environ["DESTINATION_DIR"]
 
 logging.basicConfig(
     level = logging.INFO,
@@ -22,14 +27,34 @@ def upload_file(file_content):
     output_dir = os.path.join(script_dir, "output_files")
     os.makedirs(output_dir, exist_ok=True)
     
-    file_dir = os.path.join(output_dir, "access_token.txt")
-    logger.info(f"🔄 Creating file at {file_dir}.....\n")
+    file_path = os.path.join(output_dir, "access_token.txt")
+    logger.info(f"🔄 Creating file at {file_path}.....\n")
 
     try:
-        with open(file_dir, "w", encoding="utf-8") as file:
+        with open(file_path, "w", encoding="utf-8") as file:
             file.write(file_content)
         logger.info("✅ Access token successfully written to file")
     except Exception as e:
         logger.exception("❌ File creation failed: %s", e)
         return None
+
+    # ---- Copy the file to the OneDrive folder -------
+
+    destination_dir = DESTINATION_DIR
+    os.makedirs(destination_dir, exist_ok=True)
+
+    # Copy the file to the OneDrive folder
+    destination_path = shutil.copy(file_path, destination_dir)
+    logger.info("✅ File copied to: %s", destination_path)
+
+    # ---- Deletes the original file (only if copying succeeded) ----
+    if os.path.exists(destination_path):
+        os.remove(file_path)
+        logger.info("✅ Original file deleted: %s", file_path)
+    else:
+        logger.exception("❌ Original file not found: %s", file_path)
+        return None
+    
+    
+
     
