62f1e8deb1f27d9dc99ad8930efac4ab40ed503a
dc0dr <ecdaniels826@outlook.com>
2026-05-18 21:32:51 +0000

    fix: enable load_dotenv override and set headless mode to false in login bypass utility

--- utils/login_bypass.py
+++ utils/login_bypass.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 from urllib.parse import urlparse
 
-load_dotenv()
+load_dotenv(override=True)
 USER_LOCATOR = os.environ["USER_LOCATOR"]
 USERNAME = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
@@ -28,7 +28,7 @@ def login_bypass() -> Any:
     context = p.chromium.launch_persistent_context(
         user_data_dir=profile_path,
         channel="msedge",
-        headless=True,
+        headless=False,
         args=["--start-maximized"],
         slow_mo=1500,
     )
